6b023caeb71d9efdeaa4058820a8fdc5e842c515
hrushikesh <hrushi2002j@gmail.com>
2022-02-11 02:23:52 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
+import pathlib
 from setuptools import setup
-setup(name='mysql-orm',
-    version='0.1.1',
-    description='Testing installation of Package',
-    url='https://github.com/hrushikeshj',
-    author='hrushikesh',
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='mysql-orm',
+    version='0.0.1',
+    description='An ORM for MySql.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/hrushikeshj/mysql-orm',
+    author='Hrushikesh J',
     author_email='hrushi2002j@gmail.com',
     license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
     packages=['orm'],
+    include_package_data=True,
     install_requires=[
             'mysql-connector-python',
             'pyyaml',
         ],
     setup_requires=['wheel'],
-    zip_safe=False)
+    zip_safe=False
+    )
