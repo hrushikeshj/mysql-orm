4eecd767e552a6619bf62978dcbaf8383f94a60f
hrushikesh <hrushi2002j@gmail.com>
2022-02-12 18:57:25 +0530

    workflow

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,14 +1,17 @@
 import pytest
+import os
 from orm import Table, has_many, belongs_to
 
+
 CONFIG = {
-    'host': "localhost",
-    'port': "3306",
-    'user': "root",
-    'password': "root",
-    'database': "test"
+    'host': os.environ.get('ORM_MYSQL_HOST') or 'localhost',
+    'port': os.environ.get('ORM_MYSQL_PORT') or 3306,
+    'user': os.environ.get('ORM_MYSQL_USER'),
+    'password': os.environ.get('ORM_MYSQL_PASSWORD'),
+    'database': os.environ.get('ORM_MYSQL_DATABASE')
 }
 
+print(CONFIG)
 Table.connect(config_dict=CONFIG)
 """
 Table student with following schema is alredy created in db
