ea640d98a5230bd2c8974ba5d37dd048a77b9b94
hrushikesh <hrushikeshj.201cs220@nitk.edu.in>
2022-06-26 20:35:17 +0530

    pytest

--- tests/create.sql
+++ tests/create.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE IF NOT EXISTS hrushi;
+use hrushi;
 CREATE TABLE student(id int(10) auto_increment PRIMARY KEY, name varchar(15), gender char(1), age int(3));
 Create table articles(id int(10) auto_increment PRIMARY KEY, author_id int(10),title varchar(30), content text);
 Create table authors(id int(10) auto_increment PRIMARY KEY, name varchar(30));
